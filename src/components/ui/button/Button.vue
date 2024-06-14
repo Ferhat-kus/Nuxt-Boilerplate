8f362d55e6e971fb8cfc00cb812a004c4b607601
Fatih Yıldız <47571500+0fatihyildiz@users.noreply.github.com>
2024-06-14 18:12:29 +0300

    feat: shadcn components

--- src/components/ui/button/Button.vue
+++ src/components/ui/button/Button.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import { cn } from '@/lib/utils';
-import { Primitive } from 'radix-vue';
-import type { PrimitiveProps } from 'radix-vue';
-import type { HTMLAttributes } from 'vue';
-import { type ButtonVariants, buttonVariants } from '.';
+import type { HTMLAttributes } from 'vue'
+import { Primitive, type PrimitiveProps } from 'radix-vue'
+import { type ButtonVariants, buttonVariants } from '.'
+import { cn } from '@/lib/utils'
 
 interface Props extends PrimitiveProps {
   variant?: ButtonVariants['variant']
